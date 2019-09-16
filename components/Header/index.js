55e7d2e2ab7afbc580996ecb7968ae905645ce6e
Matthew John Orth <matthewjohnorth@Matthews-MacBook-Pro-72.local>
2019-09-15 19:53:14 -0700

    ahhh almost done commit before you lose everything

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,35 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    // elements
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const heading = document.createElement('h1');
+    const temp = document.createElement('span');
+
+    // structure
+    header.appendChild(date);
+    header.appendChild(heading);
+    header.appendChild(heading);
+    header.appendChild(temp);
+
+    // content
+    date.textContent = 'MARCH 28, 2019';
+    heading.textContent = 'LambdaTimes';
+    temp.textContent = '98 degrees';
+
+    // style
+    header.classList.add('header');
+    date.classList.add('date', 'span');
+    heading.classList.add('h1');
+    temp.classList.add('temp', 'span');
+
+
+    // event handlers
+    
+    return header
+};
+
+const headerContainer = document.querySelector('.header-container');
+headerContainer.appendChild(Header());
